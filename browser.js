a6d60de783f111759a2afc113e5b4f04245af950
sethvincent <sethvincent@gmail.com>
2014-05-28 17:21:08 -0700

    v0.1.0

---
+++ browser.js
@@ -0,0 +1,89 @@
+var fs = require('fs');
+var template = require('lodash.template');
+var paganate = require('paganate');
+var eve = require('dom-events');
+var elClass = require('element-class');
+var removeEl = require('remove-elements');
+var config = require('./config.json');
+
+/* set optional host, token options from config */
+var opts = {};
+if (config.host) opts.host = config.host;
+
+/* initialize flatsheet with options */
+var flatsheet = require('flatsheet')(opts);
+var postList = [];
+
+/* set number of posts per page for pagination */
+var limit = config.limit;
+var total;
+
+/* initialize pagination */
+var page = paganate({ limit: limit });
+
+console.log('loading');
+
+/* listen for page event to page through items */
+page.on('page', function (page, offset) {
+  console.log('pagination is happening')
+  /* get items from the local db */
+  getItems(offset);
+});
+
+/* find the #get-items button */
+var getItemsEl = document.getElementById('get-items');
+
+/* listen for click event on #get-items button */
+eve.on(getItemsEl, 'click', function(e) {
+  page.next();
+  e.preventDefault();
+});
+
+/* find #main-content */
+var mainEl = document.getElementById('main-content');
+
+/* pull in html template */
+var postListSource = fs.readFileSync('views/client/post-list.html', 'utf8');
+
+/* kick off the application */
+requestData(function () {
+  console.log('first request data is happening')
+  page.page(0);
+});    
+
+
+
+/*
+* helper functions
+*/
+
+/* request data from flatsheet, plop it into indexeddb */
+function requestData (callback) {
+  flatsheet.sheet(config.sheet, function (error, response){
+    if (error) callback(error);
+    postList = response.rows.reverse();
+    total = postList.length;
+    if (callback) callback();
+  });
+}
+
+/* get items from indexeddb based on pagination offset */
+function getItems (offset) {
+  console.log(offset, limit)
+  var posts = postList.slice(offset, offset + limit);
+  console.log('get items', posts);
+  var listEl = createEl('post-list', postListSource, { posts: posts, baseurl: config.baseurl });
+  mainEl.appendChild(listEl);
+
+  if (total <= offset + limit) elClass(getItemsEl).add('hidden');
+}
+
+/* create the html for a view */
+function createEl (slug, source, data) {
+  var compiled = template(source);
+  var html = compiled(data);
+  var el = document.createElement('div');
+  el.className = slug;
+  el.innerHTML = html;
+  return el;
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+var flatsheet = require('flatsheet')();
+var express = require('express')
+var app = express();
+
+var config = require('./config.json');
+
+app.locals.baseurl = config.baseurl;
+app.use('/public', express.static(__dirname + '/public'));
+
+app.engine('.ejs', require('ejs').__express);
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res){
+  res.render('index.ejs', { posts: res });
+});
+
+app.get('/:slug', function (req, res){
+  flatsheet.sheet(config.sheet, function (err, sheet) {
+    var slug = req.params['slug'];
+
+    sheet.rows.forEach(function (row) {
+      if (row.slug == slug) {
+        res.render('post.ejs', { post: row });
+      }
+    })
+  });
+});
+
+app.listen(3000);
